cad4fae1db8ca96fa63375ab5c7d875ca331952c
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2024-04-15 17:25:42 +0200

    Fix some stuff

--- nix/default.nix
+++ nix/default.nix
@@ -6,14 +6,18 @@ in
 pkgs.stdenv.mkDerivation rec {
   pname = "mumblingdrunkard.com/master's-thesis";
   version = "0.1.0";
+  src = lib.sources.cleanSource ../.;
+
   tala = pkgs.callPackage ./tala.nix {};
+
   buildInputs = [ typst pkgs.d2 tala ];
+
   buildPhase = ''
     ${typst}/bin/typst compile ./thesis.typ
   '';
+
   installPhase = ''
     mkdir -p $out/doc
     cp thesis.pdf $out/doc/thesis.pdf
   '';
-  src = lib.sources.cleanSource ./.;
 }
